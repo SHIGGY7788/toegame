a595d2af769ae0a0dcef9cc36db244e832af8d53
Evan James Edwards <EJEdw838@district.evit.com>
2023-11-02 14:14:52 -0700

    tiktaktoe woa woa
    ai mayb probably not

---
+++ tictactoe/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      },
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
+      }
+  },
+  plugins: [],
+}
+}
